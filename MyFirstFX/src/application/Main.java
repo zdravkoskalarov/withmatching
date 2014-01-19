711309ac58775d79d9441b7a7cc941cbdb64adfd
vladimiryankov <vladimir.yankov@fdiba.tu-sofia.bg>
2014-01-19 16:30:03 +0200

    commit fxml scenes

--- MyFirstFX/src/application/Main.java
+++ MyFirstFX/src/application/Main.java
@@ -2,8 +2,10 @@ package application;
 	
 import application.dao.MySQLDAO;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -12,12 +14,27 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			/* test pane
 			BorderPane root = new BorderPane();
 			Text t = new Text("Hello FX"); //set text here
 			root.setCenter(t);
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm()); // Ooohh, CSS
 			primaryStage.setScene(scene);
+			primaryStage.show();*/
+			
+			//load home page
+			AnchorPane homePage = (AnchorPane) FXMLLoader.load(Main.class.getResource("darkHome.fxml"));
+			Scene scene = new Scene(homePage);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Home Page");
+			primaryStage.show();
+			
+			//load new scene
+			AnchorPane playPage = (AnchorPane) FXMLLoader.load(Main.class.getResource("darkPlay.fxml"));
+			scene = new Scene(playPage);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Play Test");
 			primaryStage.show();
 			
 			//try out the db connectivity
